9ce99d8f11cf054a5ddaa71176e2af04bd9a62a4
Tommy Siu <tommysiu@iafrica.com>
2019-04-16 10:43:23 +0200

    Completed browser support.

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -2,11 +2,23 @@ var gulp = require('gulp');
 var svgSprite = require('gulp-svg-sprite');
 var rename = require('gulp-rename');
 var del = require('del');
-
+var svg2png = require('gulp-svg2png');
 
 var config = {
+	shape: {
+		spacing: {
+			padding: 1
+		}
+	},
 	mode: {
 		css: {
+			variables: {
+				replaceSvgWithPng: function() {
+					return function(sprite, render) {
+						return render(sprite).split('.svg').join('.png');
+					}
+				}
+			},
 			sprite: 'sprite.svg',
 			render: {
 				css: {
@@ -28,8 +40,14 @@ gulp.task('createSprite',['beginClean'], function(){
 	.pipe(gulp.dest('./app/temp/sprite/'));
 });
 
-gulp.task('createSpriteGraphic', ['createSprite'], function() {
-	return gulp.src('./app/temp/sprite/css/**/*.svg')
+gulp.task('createPngCopy', ['createSprite'], function() {
+	return gulp.src('./app/temp/sprite/css/*.svg')
+		.pipe(svg2png())
+		.pipe(gulp.dest('./app/temp/sprite/css'));
+});
+
+gulp.task('createSpriteGraphic', ['createPngCopy'], function() {
+	return gulp.src('./app/temp/sprite/css/**/*.{svg,png}')
 	.pipe(gulp.dest('./app/assets/images/sprites'));
 });
 
@@ -43,4 +61,4 @@ gulp.task('endClean',['createSpriteGraphic', 'copySpriteCSS'], function(){
 	return del('./app/temp/sprite');
 });
 
-gulp.task('icons', ['beginClean', 'createSprite', 'createSpriteGraphic', 'copySpriteCSS', 'endClean']);
+gulp.task('icons', ['beginClean', 'createSprite', 'createPngCopy', 'createSpriteGraphic', 'copySpriteCSS', 'endClean']);
